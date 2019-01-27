edb80f845340def8dace3a4dbe2d2720ae2edcd2
Hisham Hashmi <hishamhashmi@outlook.com>
2019-01-28 00:56:49 +0500

    added expressions to types lab and string lab added

--- Types.py
+++ Types.py
@@ -21,3 +21,8 @@ print(x)
 y = (3+2)*2
 print(y)
 
+x = 3+2*2
+y = (3+2)*2
+z = x + y
+print(z)
+
---
+++ strings.py
@@ -0,0 +1,21 @@
+#defining string
+"Hisham Hashmi"
+#assign string to variables
+Name = "Hisham Hashmi"
+Name
+
+#indexing
+print(Name[0])
+print(Name[4])
+print(Name[10])
+
+#negative indexing
+print("Negative Indexing")
+print(Name[-1])
+print(Name[-5])
+print(Name[-13])
+
+#slicing
+print("Slicing")
+print(Name[0:4])
+print(Name[8:12])
