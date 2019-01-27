28493fb505965532a391c1fb13a06ae45cb3bda4
Hisham Hashmi <hishamhashmi@outlook.com>
2019-01-28 00:51:42 +0500

    Create Types.py

---
+++ Types.py
@@ -0,0 +1,23 @@
+#print hello python
+print('Hello, Python!!')
+
+#Print string and error to see the running order
+print("This will be printed")
+#frint("This will cause an error")
+print("This will not be printed")
+
+#use print() function to display Hello, World
+print('Hello, World') #Print the traditional Hello World
+
+#types
+type(-1)
+type(6/2)
+type(25//2)
+
+#expressions
+x = 3+2*2
+print(x)
+
+y = (3+2)*2
+print(y)
+
