a666dd7cea5355f0ecf2270dd0371d005e692d95
Hisham Hashmi <hishamhashmi@outlook.com>
2019-01-30 03:54:53 +0500

    Tuples.py updated

--- Tuples.py
+++ Tuples.py
@@ -62,3 +62,46 @@ print("Element 2 of Tuple",Nested1[2])
 print("Element 3 of Tuple",Nested1[3])
 print("Element 4 of Tuple",Nested1[4])
 
+#print the element on each index, including nest indexes
+print("Element 2, 0 of Tuple:", Nested1 [2][0])
+print("Element 2, 1 of Tuple:", Nested1 [2][1])
+print("Element 3, 0 of Tuple:", Nested1 [3][0])
+print("Element 3, 1 of Tuple:", Nested1 [3][1])
+print("Element 4, 0 of Tuple:", Nested1 [4][0])
+print("Element 4, 1 of Tuple:", Nested1 [4][1])
+
+#print the 1st element in the 2nd nested tuple
+print(Nested1[2][1][0])
+
+#print the 2nd element in the 2nd nested tuple
+print(Nested1[2][1][1])
+
+#print the 1st element in the 2nd nested tuple
+print(Nested1[4][1][0])
+
+#print the 2nd element in the 2nd nested tuple
+print(Nested1[4][1][1])
+
+#Quize on Tuple
+
+#Sample Tuple 
+genres_tuple = ("pop","rock","soul","hard rock","soft rock", "R&B","progressive rock","disco")
+print(len(genres_tuple))
+
+#element at index 3
+print(genres_tuple[3])
+
+#using slicing obtain elements at index 3,4,5
+print(genres_tuple[3:6])
+
+#Find first 2 elements of the tuple
+print(genres_tuple[0:2])
+
+#print the first index of "Disco"
+print(genres_tuple.index("disco"))
+
+#Generate Sorted List for Tuple
+C_tuple = (-5,1,-3)
+C_list = sorted(C_tuple)
+print(C_list)
+
