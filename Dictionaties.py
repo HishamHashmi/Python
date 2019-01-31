1775a2ff5a3727899db9ef8d60a0fe8b948083a7
Hisham Hashmi <hishamhashmi@outlook.com>
2019-02-01 02:18:47 +0500

    Dictionaries Lab

---
+++ Dictionaties.py
@@ -0,0 +1,43 @@
+# create a dictionary
+Dict = {"key1":1,"key2":"2","key3":[3,3,3],"key4":(4,4,4),('key5'):5,(0,1):6}
+print(Dict)
+
+# Access to the value by the key
+print(Dict["key1"])
+
+# Access to the value by the key
+print(Dict[(0,1)])
+
+# Create a sample dictionary
+release_year_dict = {"Thriller":"1982","Back in Black":"1980", \
+"The dark side of the moon":"1973","The Bodyguard":"1992",\
+"Bat Out of Hell":"1977","Their Greatest Hits (1971-1975)":"1976",\
+"Saturday Night Fever":"1977","Rumours":"1977"}
+print(release_year_dict)
+
+#keys
+# Get Value by keys
+print(release_year_dict['Thriller'])
+
+# Get Value by keys
+print(release_year_dict['The Bodyguard'])
+
+# Get all the keys in dictionary
+print(release_year_dict.keys())
+
+# Get all the values in dictionary
+print(release_year_dict.values())
+
+# Append value with key into dictionary
+release_year_dict['Graduation']='2007'
+print(release_year_dict)
+
+# Delete entries by key
+del(release_year_dict['Thriller'])
+del(release_year_dict['Graduation'])
+print(release_year_dict)
+
+# Verify the key is in the dictionary
+print ('The Bodyguard' in release_year_dict)
+print ('Graudation' in release_year_dict)
+
