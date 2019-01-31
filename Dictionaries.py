dfba44b5d1f3c832db65e518785825ba59f6da59
Hisham Hashmi <hishamhashmi@outlook.com>
2019-02-01 02:40:33 +0500

    Dictionaries Lab Updated

--- Dictionaries.py
+++ Dictionaries.py
@@ -41,3 +41,29 @@ print(release_year_dict)
 print ('The Bodyguard' in release_year_dict)
 print ('Graudation' in release_year_dict)
 
+# Quiz on Dictionaries
+# You will need this dictionary for the next two questions:
+# Question sample dictionary
+soundtrack_dic = {"The Bodyguard":"1992","Saturday Night Fever":"1977"}
+print(soundtrack_dic)
+
+# a) In the dictionary soundtrack_dict what are the keys ?
+print(soundtrack_dic.keys())
+
+# b) In the dictionary soundtrack_dict what are the values ?
+print(release_year_dict.values())
+
+# You will need this dictionary for the following questions:
+# The Albums Back in Black, The Bodyguard and Thriller have the following music recording sales in millions 50, 50 and 65 respectively:
+# a) Create a dictionary album_sales_dict where the keys are the album name and the sales in millions are the values.
+album_sales_dict = {"Back in Black": 50,"The Bodyguard": 50, "Thriller": 65}
+print(album_sales_dict)
+
+# b) Use the dictionary to find the total sales of Thriller:
+print(album_sales_dict["Thriller"])
+
+# c) Find the names of the albums from the dictionary using the method keys:
+print(album_sales_dict.keys())
+
+# d) Find the names of the recording sales from the dictionary using the method values:
+print(album_sales_dict.values())
